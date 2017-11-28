6ba7fffc8626a1cc392a969a9a236c1ee541e8d0
edelygomes <edelygomes@gmail.com>
2017-11-28 14:36:16 -0300

    funcao main funfando / precisa checar a insercao dos tempos dos processos

--- principal.py
+++ principal.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python
 class Algorithm():
-    '''	The algorithm choose to schedule process in a Operating System.	'''
+    '''The algorithm choosed to schedule process in a Operating System.'''
     def __init__(self, number_process=4):
         self.number_of_process = number_process
         self.process = {}
         
         for i in range(number_process):
             self.process[i] = None
-
+            
+        print(self.process)
         
     def duration_of_process(self):
         '''
@@ -16,6 +18,14 @@ class Algorithm():
             while type(self.process[k]) is not int:            
                 try:
                     self.process[k] = int(input('Insert the duration of the process {}:\n'.format(k)))
+                    print('Duration of process: {} is {}'.format(k, v))
                 except:
                     print('Duration must be an integer.')
-            print('Duration of process: {} is {}'.format(k, v))
+            
+            
+def main():
+    algo = Algorithm()
+    algo.duration_of_process()
+
+if __name__ == '__main__':
+    main()
